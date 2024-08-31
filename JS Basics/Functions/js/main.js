643642cd2944387cb12593af0a03dc5f31c95aff
PriyankaRC16 <rupali317@gmail.com>
2024-08-31 17:48:04 +0800

    Functions

---
+++ JS Basics/Functions/js/main.js
@@ -0,0 +1,15 @@
+console.log(logger('Hello'))
+console.log(add(2, 3))
+console.log(multiply(2, 3))
+
+function logger(str) {
+  console.log(str)
+}
+
+function add(num1, num2) {
+  return num1 + num2
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
