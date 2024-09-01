86d39adfa53e090a3e5967b6011cd82253d5b28d
PriyankaRC16 <rupali317@gmail.com>
2024-09-01 15:43:29 +0800

    Arrow functions: Rearranging the comments

--- JS Basics/Arrow functions/js/main.js
+++ JS Basics/Arrow functions/js/main.js
@@ -1,23 +1,21 @@
-/**
-Make a function named ten that takes in zero arguments and return the value 10. Try using both () and _ syntax.
-Make a function named logger that takes in one argument. It logs the argument you passed into it. Try it with and without parenthesis ().
-Make a function called add that adds two numbers together. Try it with and without implicit returns.
-Make a function called multiply that multiplies two numbers together. Try it with and without implicit returns.
- */
+// 1. Make a function named ten that takes in zero arguments and return the value 10. Try using both () and _ syntax.
 const tenWithUnderscore = _ => 10
 
 const tenWithBraces = () => 10
 
+// 2. Make a function named logger that takes in one argument. It logs the argument you passed into it. Try it with and without parenthesis ().
 const loggerWithoutBraces = arg => console.log(arg)
 
 const loggerWithBraces = arg => console.log(arg)
 
+// 3. Make a function called add that adds two numbers together. Try it with and without implicit returns.
 const addWithImplicitReturn = (num1, num2) => num1 + num2
 
 const addWithoutImplicitReturn = (num1, num2) => {
   return num1 + num2
 }
 
+// 4. Make a function called multiply that multiplies two numbers together. Try it with and without implicit returns.
 const multiplyWithImplicitReturn = (num1, num2) => num1 * num2
 
 const multiplyWithoutImplicitReturn = (num1, num2) => {
