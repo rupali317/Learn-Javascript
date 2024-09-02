426e6f49b63aa87b2c821db8dc80586252119cca
PriyankaRC16 <rupali317@gmail.com>
2024-09-02 23:13:47 +0800

    NOT operator

---
+++ JS Basics/Not operator/js/main.js
@@ -0,0 +1,6 @@
+console.log(!2550284) // false
+console.log(!true) // false
+console.log(!NaN) // true
+console.log(!{}) // false
+console.log(!!'Pandas are adorable!') // true
+console.log(!!'') // false
