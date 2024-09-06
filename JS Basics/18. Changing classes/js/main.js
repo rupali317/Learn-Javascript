75a8251d3c3f6f698d14a859d029d2c05fe4c07d
PriyankaRC16 <rupali317@gmail.com>
2024-09-06 21:39:58 +0800

    Changing classes

--- JS Basics/18. Changing classes/js/main.js
+++ JS Basics/18. Changing classes/js/main.js
@@ -1,2 +1,15 @@
 // Practice adding, removing, checking for classes and toggling classes with Element.classList
-// Refer to the HTML file
+const addClassElement = document.querySelector('.add')
+addClassElement.classList.add('red')
+
+const removeClassElement = document.querySelector('.remove')
+removeClassElement.classList.remove('remove')
+
+const noBlueClassElement = document.querySelector('.contains1')
+console.log(noBlueClassElement.classList.contains('blue'))
+
+const blueClassElement = document.querySelector('.contains2')
+console.log(blueClassElement.classList.contains('blue'))
+
+const toggleClassElement = document.querySelector('.toggle')
+toggleClassElement.classList.toggle('red')
