c46d79f0348beba047d3ecb9ba39c3cf15cfbdc7
rupali317 <rupali317@gmail.com>
2024-09-14 10:47:56 +0800

    Toggle menu sidebar - hiding the menu using transform and adding basic animation

--- Building simple components/01.starter/js/main.js
+++ Building simple components/01.starter/js/main.js
@@ -1,6 +1,8 @@
 const Button = document.querySelector('button')
 const NavBar = document.querySelector('nav')
+const Main = document.querySelector('main')
 
 Button.addEventListener('click', () => {
-  NavBar.classList.toggle('hidden')
+  NavBar.classList.toggle('push-offsite')
+  Main.classList.toggle('push-offsite')
 })
