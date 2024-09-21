6a130ebef1f7a74200d522469a0b9c510e5035fc
rupali317 <rupali317@gmail.com>
2024-09-21 18:57:21 +0800

    Changing CSS with javascript

---
+++ DOM basics/2. Changing CSS with Javascript/js/main.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+  button.style.color = '#fff'
+  button.style.backgroundColor = '#433458'
+  button.style.width = '4rem'
+  button.style.height = '1.5rem'
+})
