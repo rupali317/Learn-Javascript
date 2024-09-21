17c0bc94822caf54088dd15b11c6ec20fdf5046b
rupali317 <rupali317@gmail.com>
2024-09-21 17:45:32 +0800

    Finished working on the accordion:Building an accordion

--- Arrays and loops/7. Accordion/js/main.js
+++ Arrays and loops/7. Accordion/js/main.js
@@ -1,1 +1,8 @@
-// Start writing JavaScript here!
+const AccordionList = document.querySelectorAll('.accordion')
+
+AccordionList.forEach(accordion => {
+  const accordionHeader = accordion.querySelector('.accordion__header')
+  accordionHeader.addEventListener('click', () => {
+    accordion.classList.toggle('is-open')
+  })
+})
