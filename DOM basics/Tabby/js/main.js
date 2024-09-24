18a2ad57ee918f66e486f43ecf27ce76774ba28c
rupali317 <rupali317@gmail.com>
2024-09-24 23:47:22 +0800

    Tab implementation

--- DOM basics/Tabby/js/main.js
+++ DOM basics/Tabby/js/main.js
@@ -1,1 +1,17 @@
-// Start writing JavaScript here!
+const tabs = document.querySelectorAll('.tab')
+const tabContents = document.querySelectorAll('.tab-content')
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener('click', () => {
+    removeSelectionClassFromElements(tabs)
+    removeSelectionClassFromElements(tabContents)
+    tabs[index].classList.add('is-selected')
+    tabContents[index].classList.add('is-selected')
+  })
+})
+
+function removeSelectionClassFromElements(elements) {
+  elements.forEach(element => {
+    element.classList.remove('is-selected')
+  })
+}
