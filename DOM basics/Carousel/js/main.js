76395c3255ac35a92ffdde0b19e102d8f973d58a
rupali317 <rupali317@gmail.com>
2024-09-28 23:00:59 +0800

    Crafting the algorithm for the carousel

--- DOM basics/Carousel/js/main.js
+++ DOM basics/Carousel/js/main.js
@@ -1,1 +1,9 @@
-// Start writing JavaScript here!
+const nextButton = document.querySelector('.js-next-button')
+const previousButton = document.querySelector('.js-previous-button')
+
+// 1. Updating the left styles of all the carousel__slide
+//  a. Get the list of all carousel__slide, loop through them and substract 800 from their current value
+// 2. Check of the last instance of carousel__slide is reached, True -> hide the nextButton
+nextButton.addEventListener('click', () => {})
+
+previousButton.addEventListener('click', () => {})
