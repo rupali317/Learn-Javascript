2e4633a7d5679e8ac8b83fd8d96912aec1d222af
rupali317 <rupali317@gmail.com>
2024-09-29 10:31:34 +0800

    Creating carousel logic for sliding using next and previous button

--- DOM basics/Carousel/js/main.js
+++ DOM basics/Carousel/js/main.js
@@ -1,9 +1,64 @@
-const nextButton = document.querySelector('.js-next-button')
-const previousButton = document.querySelector('.js-previous-button')
+const nextButton = document.querySelector('#js-next-button')
+const previousButton = document.querySelector('#js-previous-button')
+const carouselSlideList = document.querySelectorAll('.carousel__slide')
 
-// 1. Updating the left styles of all the carousel__slide
-//  a. Get the list of all carousel__slide, loop through them and substract 800 from their current value
-// 2. Check of the last instance of carousel__slide is reached, True -> hide the nextButton
-nextButton.addEventListener('click', () => {})
+previousButton.style.display = 'none'
 
-previousButton.addEventListener('click', () => {})
+nextButton.addEventListener('click', () => {
+  previousButton.style.display = 'block'
+  carouselSlideList.forEach((currentCarouselSlide, index) => {
+    setLeftPositionOfCarouselSlides(currentCarouselSlide, true)
+    setCurrentCarouselSlideAsVisible(currentCarouselSlide)
+    setVisibilityOfNextButton(currentCarouselSlide, index)
+  })
+})
+
+previousButton.addEventListener('click', () => {
+  nextButton.style.display = 'block'
+  for (let index = carouselSlideList.length - 1; index >= 0; index--) {
+    const currentCarouselSlide = carouselSlideList[index]
+    setLeftPositionOfCarouselSlides(currentCarouselSlide, false)
+    setCurrentCarouselSlideAsVisible(currentCarouselSlide)
+    setVisibilityOfPreviousButton(currentCarouselSlide, index)
+  }
+})
+
+function setLeftPositionOfCarouselSlides(currentCarouselSlide, isSlideLeft) {
+  const leftPosition = getComputedStyle(currentCarouselSlide).left.replace(
+    'px',
+    '',
+  )
+  if (isSlideLeft) {
+    currentCarouselSlide.style.left = parseInt(leftPosition) - 800 + 'px'
+  } else {
+    currentCarouselSlide.style.left = parseInt(leftPosition) + 800 + 'px'
+  }
+}
+
+function setCurrentCarouselSlideAsVisible(currentCarouselSlide) {
+  if (currentCarouselSlide.style.left === '0px') {
+    currentCarouselSlide.dataset.shownStatus = 'true'
+  } else {
+    currentCarouselSlide.dataset.shownStatus = 'false'
+  }
+}
+
+function setVisibilityOfNextButton(currentCarouselSlide, index) {
+  if (index === carouselSlideList.length - 1) {
+    if (currentCarouselSlide.dataset.shownStatus === 'true') {
+      nextButton.style.display = 'none'
+    }
+  } else {
+    nextButton.style.display = 'block'
+  }
+}
+
+function setVisibilityOfPreviousButton(currentCarouselSlide, index) {
+  if (index === 0) {
+    if (currentCarouselSlide.dataset.shownStatus === 'true') {
+      previousButton.style.display = 'none'
+    }
+  } else {
+    previousButton.style.display = 'block'
+  }
+}
