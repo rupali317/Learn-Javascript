24d5ce5fd2cb8c4c9240fe1f5bc02398f1e159da
rupali317 <rupali317@gmail.com>
2024-10-19 16:45:11 +0800

    Taking inspiration from Zell's method

--- DOM basics/Carousel/js/main_v2.js
+++ DOM basics/Carousel/js/main_v2.js
@@ -1,1 +1,44 @@
-// Zell's method
+// Taking inspiration from Zell's method
+const carousel = document.querySelector('.carousel')
+const nextButton = carousel.querySelector('#js-next-button')
+const prevButton = carousel.querySelector('#js-previous-button')
+const carouselSlides = Array.from(carousel.querySelectorAll('.carousel__slide'))
+const slideWidth = parseInt(
+  getComputedStyle(carouselSlides[0]).width.replace('px', ''),
+)
+
+nextButton.addEventListener('click', () => {
+  prevButton.removeAttribute('hidden')
+  const currentSlide = carousel.querySelector('.is-selected')
+  const nextSlide = currentSlide.nextElementSibling
+  for (let i = 0; i < carouselSlides.length; i++) {
+    const carouselSlide = carouselSlides[i]
+    carouselSlide.style.left =
+      getLeftPositionOfSlide(carouselSlide) - slideWidth + 'px'
+  }
+  currentSlide.classList.remove('is-selected')
+  nextSlide.classList.add('is-selected')
+  if (!nextSlide.nextElementSibling) {
+    nextButton.setAttribute('hidden', '')
+  }
+})
+
+prevButton.addEventListener('click', () => {
+  nextButton.removeAttribute('hidden')
+  const currentSlide = carousel.querySelector('.is-selected')
+  const prevSlide = currentSlide.previousElementSibling
+  for (let i = carouselSlides.length - 1; i >= 0; i--) {
+    const carouselSlide = carouselSlides[i]
+    carouselSlide.style.left =
+      getLeftPositionOfSlide(carouselSlide) + slideWidth + 'px'
+  }
+  currentSlide.classList.remove('is-selected')
+  prevSlide.classList.add('is-selected')
+  if (!prevSlide.previousElementSibling) {
+    prevButton.setAttribute('hidden', '')
+  }
+})
+
+function getLeftPositionOfSlide(carouselSlide) {
+  return parseInt(getComputedStyle(carouselSlide).left.replace('px', ''))
+}
