79bed979fccc85a16554bc7ffef53d97660c7e1d
rupali317 <rupali317@gmail.com>
2024-11-03 15:59:42 +0800

    Practicing event propagation

--- Events/3. Event propagation/main.js
+++ Events/3. Event propagation/main.js
@@ -1,17 +1,33 @@
-const boxes = document.querySelectorAll('.box')
+const box1 = document.querySelector('.box1')
+const box2 = document.querySelector('.box2')
+const box3 = document.querySelector('.box3')
 
-boxes.forEach(box => {
-  box.addEventListener('click', e => {
-    console.log(e.eventPhase + '' + e.currentTarget)
-  })
+box1.addEventListener('click', () => {
+  console.log('box 1 clicked')
 })
 
-// boxes.forEach(box => {
-//   box.addEventListener(
-//     'click',
-//     e => {
-//       console.log(e.eventPhase + '' + e.currentTarget)
-//     },
-//     true,
-//   )
-// })
+// Add an event listener in the capturing phase
+box2.addEventListener(
+  'click',
+  () => {
+    console.log('box 2 clicked')
+  },
+  true,
+)
+
+box3.addEventListener('click', e => {
+  console.log('box 3 is clicked')
+  //   e.stopPropagation()  This will prevent the other 2 consoles from being logged
+  e.stopImmediatePropagation()
+})
+
+// Familiarize yourself with the sequence of events that occur.
+
+// Add an event listener in the bubbling phase
+// Answer these questions:
+
+// Which phase comes first? The capturing phase or the bubbling phase? Capturing
+// What event listeners are fired in the capturing phase? all event listeners with useCapture as true
+// What event listeners are fired in the target phase? all event listeners listening to the element
+// What event listeners are fired in the bubbling phase? all event listeners with useCapture as false
+// How do you stop an event from bubbling? e.stopPropagation()
---
+++ Events/3. Event propagation/main_v2.js
@@ -0,0 +1,17 @@
+const boxes = document.querySelectorAll('.box')
+
+boxes.forEach(box => {
+  box.addEventListener('click', e => {
+    console.log(e.eventPhase + '' + e.currentTarget)
+  })
+})
+
+boxes.forEach(box => {
+  box.addEventListener(
+    'click',
+    e => {
+      console.log(e.eventPhase + '' + e.currentTarget)
+    },
+    true,
+  )
+})
