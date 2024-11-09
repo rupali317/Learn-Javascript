56c6c4c8e08e20397523ab93a526797428353b69
rupali317 <rupali317@gmail.com>
2024-11-09 23:06:35 +0800

    Event delegation

--- Events/4. Event delegation/main.js
+++ Events/4. Event delegation/main.js
@@ -1,5 +1,19 @@
 const list = document.querySelector('ul')
+const button = document.querySelector('button')
 
 list.addEventListener('click', e => {
-  console.log(e.target)
+  if (e.target.matches('li')) console.log(e.target)
+  //console.log(e.target) // will log other like li, a, ul
 })
+
+list.addEventListener('click', e => {
+  if (e.target.closest('ul')) {
+    console.log('it is ul')
+  }
+})
+
+button.addEventListener('click', logging)
+
+function logging(e) {
+  console.log(e.target)
+}
