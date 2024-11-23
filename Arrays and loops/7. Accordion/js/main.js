5145ed289901370896227462d0f154b15465632f
rupali317 <rupali317@gmail.com>
2024-11-23 21:14:55 +0800

    Accordion: Event delegation

--- Arrays and loops/7. Accordion/js/main.js
+++ Arrays and loops/7. Accordion/js/main.js
@@ -1,8 +1,21 @@
-const AccordionList = document.querySelectorAll('.accordion')
+/* METHOD 1: Loop and attach event listeners to each of the headers
+
+const AccordionList = Array.from(document.querySelectorAll('.accordion'))
 
 AccordionList.forEach(accordion => {
   const accordionHeader = accordion.querySelector('.accordion__header')
   accordionHeader.addEventListener('click', () => {
     accordion.classList.toggle('is-open')
-  })
+    })
+}) */
+
+// METHOD 2: Event delegation
+
+const AccordionContainer = document.querySelector('.accordion-container')
+
+AccordionContainer.addEventListener('click', e => {
+  if (e.target.matches('.accordion__header button')) {
+    const accordion = e.target.closest('.accordion')
+    accordion.classList.toggle('is-open')
+  }
 })
