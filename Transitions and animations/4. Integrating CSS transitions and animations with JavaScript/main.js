bf21f312715e43b73f3d8a498f03a8189fbf6017
rupali317 <rupali317@gmail.com>
2024-12-08 22:13:46 +0800

    Created two more elements with two different animations. Used animationend to remove elements when their transition is completes.

--- Transitions and animations/4. Integrating CSS transitions and animations with JavaScript/main.js
+++ Transitions and animations/4. Integrating CSS transitions and animations with JavaScript/main.js
@@ -1,6 +1,8 @@
 const buttonOne = document.querySelector('#button-one')
 const buttonTwo = document.querySelector('#button-two')
 const buttonThree = document.querySelector('#button-three')
+const buttonFour = document.querySelector('#button-four')
+const buttonFive = document.querySelector('#button-five')
 
 // Use transitionend to remove elements when their transition is completes.
 
@@ -15,4 +17,12 @@ buttonTwo.addEventListener('transitionend', () => {
 buttonThree.addEventListener('transitionend', () => {
   console.log('Button three is removed') // will be logged 4 times since this is about the borders on all 4 directions
   buttonThree.remove()
+})
+
+buttonFour.addEventListener('animationend', () => {
+  buttonFour.remove()
+})
+
+buttonFive.addEventListener('animationend', () => {
+  buttonFive.remove()
 })
