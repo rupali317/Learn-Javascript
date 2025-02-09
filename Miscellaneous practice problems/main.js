fe6416c78991bbd1556ac93cbe01258fdd3749d3
rupali317 <rupali317@gmail.com>
2025-02-09 19:45:24 +0800

    Getting data from wiki

--- Miscellaneous practice problems/main.js
+++ Miscellaneous practice problems/main.js
@@ -1,1 +1,21 @@
-function getDataFromWiki() {}
+const submitButton = document.querySelector('button')
+const input = document.querySelector('input')
+const URL = submitButton.addEventListener('click', getDataFromWiki)
+
+function getDataFromWiki() {
+  const topic = input.value
+  const URL = `https://en.wikipedia.org/w/api.php?action=parse&section=0&prop=text&format=json&page=${topic}`
+  fetch(URL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`)
+      }
+      return response.json()
+    })
+    .then(data => {
+      console.log(`data received: ${data}`)
+    })
+    .catch(error => {
+      console.log(`Error fetching: ${error}`)
+    })
+}
