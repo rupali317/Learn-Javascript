302b2aa62ff2b9637619a0c526528daeed5388cb
rupali317 <rupali317@gmail.com>
2025-02-16 22:42:00 +0800

    Print the total number of times that the string [topic] appears in the article's text field.

--- Miscellaneous practice problems/Retrieve and manipulate data from Wiki/main.js
+++ Miscellaneous practice problems/Retrieve and manipulate data from Wiki/main.js
@@ -1,21 +1,28 @@
 const submitButton = document.querySelector('button')
 const input = document.querySelector('input')
-const URL = submitButton.addEventListener('click', getDataFromWiki)
+const fetchedData = submitButton.addEventListener('click', getDataFromWiki)
+const countText = document.querySelector('#js-count-occurence')
 
 function getDataFromWiki() {
   const topic = input.value
+  let count = 0
   const URL = `http://localhost:3000/wiki?topic=${topic}`
-  fetch(URL)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`)
-      }
-      return response.json()
-    })
-    .then(data => {
-      console.log(`data received: ${data}`)
-    })
-    .catch(error => {
-      console.log(`Error fetching: ${error}`)
-    })
+  // fetch(URL)
+  //   .then(response => {
+  //     if (!response.ok) {
+  //       throw new Error(`HTTP error! Status: ${response.status}`)
+  //     }
+  //     return response.json()
+  //   })
+  //   .then(data => {
+  const data =
+    'This is how Singapore is. Singapore is beautiful. Singapore is spectacular'
+  const topicRegex = `${topic}`
+  const topicRegexStringIterator = data.matchAll(topicRegex)
+  topicRegexStringIterator.forEach(_ => count++)
+  countText.textContent = `Number of times ${topic} appeared = ${count}`
+  // })
+  // .catch(error => {
+  //   console.log(`Error fetching: ${error}`)
+  // })
 }
