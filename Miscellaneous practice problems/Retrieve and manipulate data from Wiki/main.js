ecaadca163cdd5ca911ed9a653f5c969c1125562
rupali317 <rupali317@gmail.com>
2025-02-16 22:44:24 +0800

    Uncommenting the changes related to fetch-related code

--- Miscellaneous practice problems/Retrieve and manipulate data from Wiki/main.js
+++ Miscellaneous practice problems/Retrieve and manipulate data from Wiki/main.js
@@ -7,22 +7,22 @@ function getDataFromWiki() {
   const topic = input.value
   let count = 0
   const URL = `http://localhost:3000/wiki?topic=${topic}`
-  // fetch(URL)
-  //   .then(response => {
-  //     if (!response.ok) {
-  //       throw new Error(`HTTP error! Status: ${response.status}`)
-  //     }
-  //     return response.json()
-  //   })
-  //   .then(data => {
-  const data =
-    'This is how Singapore is. Singapore is beautiful. Singapore is spectacular'
-  const topicRegex = `${topic}`
-  const topicRegexStringIterator = data.matchAll(topicRegex)
-  topicRegexStringIterator.forEach(_ => count++)
-  countText.textContent = `Number of times ${topic} appeared = ${count}`
-  // })
-  // .catch(error => {
-  //   console.log(`Error fetching: ${error}`)
-  // })
+  fetch(URL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`)
+      }
+      return response.json()
+    })
+    .then(data => {
+      const data =
+        'This is how Singapore is. Singapore is beautiful. Singapore is spectacular'
+      const topicRegex = `${topic}`
+      const topicRegexStringIterator = data.matchAll(topicRegex)
+      topicRegexStringIterator.forEach(_ => count++)
+      countText.textContent = `Number of times ${topic} appeared = ${count}`
+    })
+    .catch(error => {
+      console.log(`Error fetching: ${error}`)
+    })
 }
