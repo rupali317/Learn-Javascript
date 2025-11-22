bc92c4e4d295198cc9f11ae77f89d87780341da6
rupali317 <rupali317@gmail.com>
2025-11-22 15:02:16 +0800

    Symbols and serailisation concept

--- JS Basics/6. Strings, numbers and booleans/js/main.js
+++ JS Basics/6. Strings, numbers and booleans/js/main.js
@@ -40,3 +40,20 @@ console.log('Min safe integer' + Number.MIN_SAFE_INTEGER) // -9007199254740991
 /* BigInt is needed for precious calculation otherwise with number inaccurate calculation happens */
 console.log(Number.MAX_SAFE_INTEGER + 30) //9007199254741020
 console.log(BigInt(9007199254740991) + BigInt(30)) //9007199254741020n . The 'n' distingusihes Number from BigInt
+
+/* Symbols are guaranteed-unique tokens you can use as object property keys that won't clash with any other properties. */
+console.log(typeof Symbol('foo'))
+const sym1 = Symbol()
+const sym2 = Symbol()
+console.log(sym1 === sym2)
+
+// Serialisation is changing into string
+
+/*
+We cannot send object over HTTP. we can send strings
+
+They can't be serialized because:
+- Symbols: Lose their meaning outside the original runtime context
+- BigInt: Not part of the JSON standard specification
+- Functions: Represent executable code, which is a security risk and loses execution context. It is more of behaviour not a data. If function has deletefiles operation and runs in another environment -> this will be risky
+*/
