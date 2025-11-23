9a0b2c50a94983946e7aad286e6de3ebc3f34705
rupali317 <rupali317@gmail.com>
2025-11-23 11:23:05 +0800

    [JS Basic] function expression

--- JS Basics/8. Understanding Functions/js/main.js
+++ JS Basics/8. Understanding Functions/js/main.js
@@ -1,6 +1,7 @@
 console.log(logger('Hello'))
 console.log(add(2, 3))
 console.log(multiply(2, 3))
+console.log(substract(2, 3))
 
 function logger(str) {
   console.log(str)
@@ -10,6 +11,23 @@ function add(num1, num2) {
   return num1 + num2
 }
 
+function substract(num1, num2) {
+  return num1 - num2
+}
+
 function multiply(num1, num2) {
   return num1 * num2
 }
+
+console.log(typeof multiply)
+
+// console.log(funcExp) TODO: explore hoising for function and variables
+
+const funcExp = function name() {
+  // without 'name' it is anonymous function, otheriwse it is a named function
+  console.log(typeof name) // accessible here but not outside
+  return 'I am a function expression'
+}
+
+console.log(funcExp())
+// console.log(name())
