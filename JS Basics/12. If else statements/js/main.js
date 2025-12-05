04664ce829290de5de3cc7a22167ed234874bd6e
rupali317 <rupali317@gmail.com>
2025-12-05 09:43:22 +0800

    if/else exercise

--- JS Basics/12. If else statements/js/main.js
+++ JS Basics/12. If else statements/js/main.js
@@ -1,30 +1,31 @@
+const myAge = 32
 const james = 22
 const valerie = 25
 const kenneth = 27
 
-const myAge = 27
-
-if (myAge < james) {
-  console.log('I am younger than James')
+if (mAge < james) {
+  console.log('I am younger than james')
 } else if (myAge > valerie) {
-  console.log('I am older than Valerie')
+  console.log('I am older than valerie')
 } else if (myAge === kenneth) {
-  console.log('Kenneth and I are of the same age')
-} else if (myAge !== kenneth) {
-  console.log('Kenneth and I are not of the same age')
+  console.log('I am as old as kenneth')
+} else {
+  console.log('Else block')
 }
 
-/**
- 1. else
- 2. if
- 3. else
- 4. else
- 5. else
- 6. if
- 7. else
- 8. if
- 9. if
- 10. if
- 11. if
- 12. if
- */
+/*
+- someValue evaluates to true
+- someValue evaluates to false
+1. else
+2. if
+3. else
+4. else
+5. else
+6. if
+7. else
+8. if
+9. if
+10. if
+11. if
+12. if
+*/
