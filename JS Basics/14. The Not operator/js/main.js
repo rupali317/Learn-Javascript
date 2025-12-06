49ef472c9ad4ccaa427c9b209dfd6f455e8586a0
rupali317 <rupali317@gmail.com>
2025-12-06 19:17:52 +0800

    NOT operator

--- JS Basics/14. The Not operator/js/main.js
+++ JS Basics/14. The Not operator/js/main.js
@@ -1,6 +1,6 @@
-console.log(!2550284) // false
-console.log(!true) // false
-console.log(!NaN) // true
-console.log(!{}) // false
-console.log(!!'Pandas are adorable!') // true
-console.log(!!'') // false
+console.log(!2550284) // 2550284 = true, !2550284 = false
+console.log(!true) // true = true, !true = false
+console.log(!NaN) // NaN = false, !NaN = true
+console.log(!{}) // {} = true, !{} = false
+console.log(!!'Pandas are adorable!') // 'Pandas are adorable!' = true, !'Pandas are adorable!' = false, !!'Pandas are adorable!' = true
+console.log(!!'') // '' = false, !'' = true, !!'' = false
