d01952adbe2eb02180ef1abc3843c7444095ccaf
rupali317 <rupali317@gmail.com>
2025-12-07 21:08:22 +0800

    [JS Basics] BOM and DOM

---
+++ JS Basics/16. The BOM and the DOM/main.js
@@ -0,0 +1,8 @@
+console.log(window.document)
+console.log(document)
+
+/*
+BOM = Browser Object Model = Javascripts objects provided by the browser
+DOM = Document Object Model = the HTML structure represented as tree of objects
+The DOM organizes the document as a hierarchical tree, with the document object at the root.
+*/
