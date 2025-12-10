4060d913e3dc54b483e979931cce389829211032
rupali317 <rupali317@gmail.com>
2025-12-10 09:56:21 +0800

    [JS Basics] Listening to events - Change the background color of button and body when the button is clicked

--- JS Basics/19. Listening to events/js/main.js
+++ JS Basics/19. Listening to events/js/main.js
@@ -1,11 +1,4 @@
 const button = document.querySelector('button')
 button.addEventListener('click', function () {
-  // console.log('button is clicked')
-  /* const body = document.body
-  if (body.classList.contains('is-clicked')) {
-    body.classList.remove('is-clicked')
-  } else {
-    body.classList.add('is-clicked')
-  } */
-  document.body.classList.toggle('is-clicked')
+  button.classList.toggle('is-clicked')
 })
