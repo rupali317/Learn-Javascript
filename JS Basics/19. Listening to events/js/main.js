554b31cd10b16902f3ab7a9410438613bd4399d5
rupali317 <rupali317@gmail.com>
2025-12-10 22:38:47 +0800

    [Js Basic] Events exercise

--- JS Basics/19. Listening to events/js/main.js
+++ JS Basics/19. Listening to events/js/main.js
@@ -1,4 +1,9 @@
 const button = document.querySelector('button')
 button.addEventListener('click', function () {
+  // if (button.classList.contains('is-clicked')) {
+  //   button.classList.remove('is-clicked')
+  // } else {
+  //   button.classList.add('is-clicked')
+  // }
   button.classList.toggle('is-clicked')
 })
