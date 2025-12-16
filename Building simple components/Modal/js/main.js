5073bd9601c2b91ef9b204c93f10cb40ea910c23
rupali317 <rupali317@gmail.com>
2025-12-16 22:40:26 +0800

    [Building simple components] Modal

--- Building simple components/Modal/js/main.js
+++ Building simple components/Modal/js/main.js
@@ -1,1 +1,9 @@
-// Start writing JavaScript here!
+const jsModalButton = document.querySelector('.jsModalButton')
+const jsModalOverlay = document.querySelector('.jsModalOverlay')
+const jsModalClose = document.querySelector('.jsModalClose')
+jsModalButton.addEventListener('click', () => {
+  jsModalOverlay.classList.add('open-mode')
+})
+jsModalClose.addEventListener('click', () => {
+  jsModalOverlay.classList.remove('open-mode')
+})
