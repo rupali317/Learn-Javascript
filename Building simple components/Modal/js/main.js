8ebd26f76a00829e06e9b3e79975578d286d438b
rupali317 <rupali317@gmail.com>
2025-12-17 20:06:57 +0800

    [Building simple components] Manage visibility of dialog when clicked on it and outside as well

--- Building simple components/Modal/js/main.js
+++ Building simple components/Modal/js/main.js
@@ -1,9 +1,18 @@
 const jsModalButton = document.querySelector('.jsModalButton')
 const jsModalOverlay = document.querySelector('.jsModalOverlay')
-const jsModalClose = document.querySelector('.jsModalClose')
+const jsModalClose = jsModalOverlay.querySelector('.jsModalClose')
+const modal = jsModalOverlay.querySelector('.modal')
+const modalIcon = jsModalOverlay.querySelector('.wave-hand')
+
 jsModalButton.addEventListener('click', () => {
   jsModalOverlay.classList.add('open-mode')
 })
-jsModalClose.addEventListener('click', () => {
-  jsModalOverlay.classList.remove('open-mode')
-})
+
+jsModalClose.addEventListener('click', manageVisibilityModalOverlay)
+jsModalOverlay.addEventListener('click', manageVisibilityModalOverlay)
+
+function manageVisibilityModalOverlay(e) {
+  if (e.target !== modal && e.target !== modalIcon) {
+    jsModalOverlay.classList.remove('open-mode')
+  }
+}
