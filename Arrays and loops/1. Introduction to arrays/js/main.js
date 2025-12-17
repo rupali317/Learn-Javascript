3f8f3948f5f2e33b80a077b4f2141bfe8d5dc920
rupali317 <rupali317@gmail.com>
2025-12-17 21:43:32 +0800

    [Arrays and loops] Introduction to arrays

--- Arrays and loops/1. Introduction to arrays/js/main.js
+++ Arrays and loops/1. Introduction to arrays/js/main.js
@@ -1,17 +1,13 @@
 //  Make an empty array that contains nothing.
-const emptyArray = []
-console.log(emptyArray)
+const emptyArr = []
 
 // Make an array that contains three items.
-const Items = [1, 2, 3]
-console.log(Items)
+const arr = [1, 2, 3]
 
 /*  Given this a list of people (below), do the following:
 Get Franklin Roosevelt from the people array
 Set Thomas Edison to Inventor of the lightbulb
 */
-const people = ['Franklin Roosevelt', 'Thomas Edison', 'Benjamin Franklin']
+let people = ['Franklin Roosevelt', 'Thomas Edison', 'Benjamin Franklin']
 console.log(people[0])
 people[1] = 'Inventor of the lightbulb'
-console.log(people[1])
-console.log(people)
