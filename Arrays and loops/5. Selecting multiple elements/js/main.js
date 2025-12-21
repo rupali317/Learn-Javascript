f4803ea27e8af24f00b4fe9d178dcaece797e19d
rupali317 <rupali317@gmail.com>
2025-12-21 21:32:18 +0800

    [Arrays and loops] Selecting multiple elements

--- Arrays and loops/5. Selecting multiple elements/js/main.js
+++ Arrays and loops/5. Selecting multiple elements/js/main.js
@@ -1,11 +1,20 @@
 // 1. Select all good guys with attributes
+const AllGoodGuys = document.querySelectorAll("[data-type='good-guy']")
+console.log(AllGoodGuys)
 
 // 2. Give good guys a yay class
+AllGoodGuys.forEach(GoodGuy => GoodGuy.classList.add('yay'))
 
 // 3. Select all villains with attributes
+const AllVillains = document.querySelectorAll("[data-type='villain']")
+console.log(AllVillains)
 
 // 4. Give villains a nay class
+AllVillains.forEach(villain => villain.classList.add('nay'))
 
 // 5. Select all characters through the character class
+const AllCharacters = document.querySelectorAll('.character')
+console.log(AllCharacters)
 
 // 6. Give all characters a star-wars class
+AllCharacters.forEach(character => character.classList.add('star-wars'))
