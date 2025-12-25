58a4ef3a6c816a8a8860d807ed913b248fbb3829
rupali317 <rupali317@gmail.com>
2025-12-25 14:36:42 +0800

    [Getting CSS with Javascript] Exercise

--- DOM basics/3. Getting CSS with Javascript/js/main.js
+++ DOM basics/3. Getting CSS with Javascript/js/main.js
@@ -1,12 +1,21 @@
-// What’s the difference between the values of these properties?
+// Difference between style property and getComputedStyle
+// 1. Editing via style object is possible but editing via getComputedStyle is not
+// 2. style object is for inline style and if an inline style is not present then style object's
+// property (eg element.style.fontColor) will be '', for css in separate file, we use getComputedStyle
+// 3. The values in properties of style object will be presented as it is. For getComputedStyle relative units get converted to absolute ones such as rem to px
 
-const divElement = document.querySelector('div')
+const container = document.querySelector('div')
+const paragraph = document.querySelector('p')
 
-console.log(divElement.style.color) // red
-console.log(divElement.style.backgroundColor) // white
-console.log(divElement.style.fontSize) // 5em
+// Get styles with style object
+console.log(container.style.fontSize) // 5em
+console.log(container.style.backgroundColor) // white
+console.log(container.style.color) // red
 
-const style = getComputedStyle(divElement)
-console.log(style.color) // rgb(255, 0, 0)
-console.log(style.backgroundColor) // rgb(255, 255, 255)
-console.log(style.fontSize) // 80px
+// Get styles with getComputedStyle
+const computedStyle = getComputedStyle(container)
+console.log(computedStyle.fontSize) // 80px
+console.log(computedStyle.backgroundColor) // rgb(255, 255, 255)
+console.log(computedStyle.color) // rgb(255, 0, 0)
+
+// Further experiments with <p> element
