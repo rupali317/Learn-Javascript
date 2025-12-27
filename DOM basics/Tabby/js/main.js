b22803c793a58ae9159ac950a0e1f2ae2ad1b9df
rupali317 <rupali317@gmail.com>
2025-12-27 20:14:59 +0800

    [Tabby] Refactoring the code

--- DOM basics/Tabby/js/main.js
+++ DOM basics/Tabby/js/main.js
@@ -1,24 +1,20 @@
-// 1. Capture selected data by default via data-theme
+// 1. Capture selected data by default
 // 2. Attach event listeners to button -> "tab". In the handler we add "is-selected" to the new one
 // and remove from the previous selected one and assign the current one
 
-let theme
-let selectedTab
-let selectedIndex
+let prevSelectedIndex
 const tabs = Array.from(document.querySelector('.tabs').children)
 const tabContents = Array.from(document.querySelector('.tab-contents').children)
-tabs.forEach((tab, index) => {
+
+tabs.forEach((tab, currIndex) => {
   if (tab.classList.contains('is-selected')) {
-    theme = tab.dataset.theme
-    selectedTab = tab
-    selectedIndex = index
+    prevSelectedIndex = currIndex
   }
   tab.addEventListener('click', () => {
     tab.classList.add('is-selected')
-    tabContents[index].classList.add('is-selected')
-    tabContents[selectedIndex].classList.remove('is-selected')
-    selectedTab.classList.remove('is-selected')
-    selectedTab = tab
-    selectedIndex = index
+    tabs[prevSelectedIndex].classList.remove('is-selected')
+    tabContents[currIndex].classList.add('is-selected')
+    tabContents[prevSelectedIndex].classList.remove('is-selected')
+    prevSelectedIndex = currIndex
   })
 })
