4f581f8ad7d73b43dcb0d9b5e961b07ec6c28ae9
rupali317 <rupali317@gmail.com>
2026-01-01 15:49:41 +0800

    [Carousel] Instead of looping through each carousel slide, we apply the left on the oevrall carousel slide content without modifying the left of each carousel slide

--- DOM basics/Carousel/My solution v3/js/main.js
+++ DOM basics/Carousel/My solution v3/js/main.js
@@ -1,39 +1,44 @@
-// Improvements as suggested by AI:
+/* Improvements as suggested by AI:
+1. Instead of looping through each carousel slide, we apply the left on the oevrall carousel slide content without modifying
+the left of each carousel slide
+*/
 const carousel = document.querySelector('.carousel')
 const nextBtn = carousel.querySelector('#next-btn')
 const prevBtn = carousel.querySelector('#prev-btn')
+const carouselContents = carousel.querySelector('.carousel__contents')
 const carouselSlides = carousel.querySelectorAll('.carousel__slide')
 const carouselDots = carousel.querySelectorAll('.carousel__dot')
 let selectedDot = carousel.querySelector('.carousel__dot.is-selected')
 
-let currImageNum = parseInt(nextBtn.dataset.currImageNum)
 let slideWidth = parseInt(getComputedStyle(carouselSlides[0]).width)
 const totalSlides = carouselSlides.length
 
 nextBtn.addEventListener('click', () => {
-  currImageNum += 1
   selectedDot.classList.remove('is-selected')
   selectedDot.nextElementSibling.classList.add('is-selected')
   selectedDot = selectedDot.nextElementSibling
-  manageVisibilityOfNavigationButtons()
-  carouselSlides.forEach(carouselSlide => {
-    let left = parseInt(getComputedStyle(carouselSlide).left)
-    let NewLeft = left - slideWidth
-    carouselSlide.style.left = NewLeft + 'px'
-  })
+  let currentSlide = carouselContents.querySelector('.is-selected') // Improvement 1
+  let nextSlide = currentSlide.nextElementSibling
+  prevBtn.hidden = false
+  nextBtn.hidden = nextSlide.nextElementSibling === null
+  let left = getComputedStyle(nextSlide).left
+  carouselContents.style.left = '-' + left
+  currentSlide.classList.remove('is-selected')
+  nextSlide.classList.add('is-selected')
 })
 
 prevBtn.addEventListener('click', () => {
-  currImageNum -= 1
   selectedDot.classList.remove('is-selected')
   selectedDot.previousElementSibling.classList.add('is-selected')
   selectedDot = selectedDot.previousElementSibling
-  manageVisibilityOfNavigationButtons()
-  carouselSlides.forEach(carouselSlide => {
-    let left = parseInt(getComputedStyle(carouselSlide).left)
-    let NewLeft = left + slideWidth
-    carouselSlide.style.left = NewLeft + 'px'
-  })
+  let currentSlide = carouselContents.querySelector('.is-selected') // Improvement 1
+  let prevSlide = currentSlide.previousElementSibling
+  nextBtn.hidden = false
+  prevBtn.hidden = prevSlide.previousElementSibling === null
+  let left = getComputedStyle(prevSlide).left
+  carouselContents.style.left = '-' + left
+  currentSlide.classList.remove('is-selected')
+  prevSlide.classList.add('is-selected')
 })
 
 carouselDots.forEach((carouselDot, index) => {
