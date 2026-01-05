cc6d11de750a21a872aaf6e7b9bb54acc38bbc43
rupali317 <rupali317@gmail.com>
2026-01-05 23:31:08 +0800

    [Listening element] exercise

--- Events deep dive/1. Listening element/main.js
+++ Events deep dive/1. Listening element/main.js
@@ -1,6 +1,8 @@
 const button = document.querySelector('button')
 
 button.addEventListener('click', () => {
+  console.log('button.style = ')
+  console.log(button.style)
   button.style.backgroundColor = 'red'
 })
 
@@ -11,27 +13,42 @@ button.addEventListener('click', () => {
 button.addEventListener('click', changeColor)
 
 button.addEventListener('click', e => {
+  console.log('arrow function = ')
   console.log(this)
+  console.log('arrow function = ')
   console.log(e)
+  console.log('arrow function = ')
   console.log(e.currentTarget)
   console.log('Button clicked')
 })
 
 function changeColor(e) {
+  console.log('change color function = ')
   console.log(this)
+  console.log('change color function = ')
   console.log(e)
+  console.log('change color function = ')
   console.log(e.currentTarget)
   button.style.color = 'blue'
 }
 
 const inputElement = document.querySelector('input')
 inputElement.addEventListener('click', e => {
-  console.log('arrow function')
+  console.log('input element arrow function = ')
   console.log(this) // arrow function -> global this
   console.log(e.currentTarget)
 })
 
 inputElement.addEventListener('click', function (e) {
-  console.log('normal function = ')
+  console.log('input element normal function = ')
   console.log(this) // normal function, this will be the input
 })
+
+const changeInputColor = e => {
+  console.log('input element arrow function changeInputColor = ')
+  console.log(this) // arrow function -> global this
+  console.log(e.target)
+  e.target.style.backgroundColor = 'yellow'
+}
+
+inputElement.addEventListener('click', changeInputColor)
