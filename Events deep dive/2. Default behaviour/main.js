32495ec252cffa528a33025a5de59130edfc1281
rupali317 <rupali317@gmail.com>
2026-01-06 22:59:43 +0800

    [Default behaviour] Insert explanations

--- Events deep dive/2. Default behaviour/main.js
+++ Events deep dive/2. Default behaviour/main.js
@@ -3,12 +3,12 @@ const checkbox = document.querySelector('input')
 
 link.addEventListener('click', e => {
   console.log(e.defaultPrevented) // is false
-  e.preventDefault()
-  console.log(e.defaultPrevented) // is true
+  e.preventDefault() // when default action is prevented, browsers set the defaultPrevented property to true
+  console.log(e.defaultPrevented) // is true. After this, the link's default action of navigating to google.com will not happen
 })
 
 checkbox.addEventListener('click', e => {
   console.log(e.defaultPrevented)
   e.preventDefault()
-  console.log(e.defaultPrevented)
+  console.log(e.defaultPrevented) // After this, the default action of checking will not work
 })
