35f2ba72daae3164260dbbcabd6912fcfa837448
rupali317 <rupali317@gmail.com>
2026-01-08 22:52:37 +0800

    [Event propagation] Created a JS notes explaining when the bubbling and capturing occur when e.eventPhase is logged

---
+++ Events deep dive/3. Event propagation/main_v3.js
@@ -0,0 +1,33 @@
+const box1 = document.querySelector('.box1')
+const box2 = document.querySelector('.box2')
+const box3 = document.querySelector('.box3')
+const box4 = document.querySelector('.box4')
+
+box1.addEventListener('click', e => console.log('box1 clicked', e.eventPhase))
+
+box2.addEventListener('click', e => console.log('box2 clicked', e.eventPhase))
+
+box3.addEventListener('click', e => console.log('box3 clicked', e.eventPhase))
+
+box4.addEventListener('click', e => console.log('box4 clicked', e.eventPhase))
+
+/*
+NOTES on e.eventPhase:
+1 = capturing 
+2 = target
+3 = bubbling
+
+when usecapture argument is false or left blank then when you click on a child box, bubbling will occur from child to parent
+
+when you click on box 4, the output will be:
+box4 clicked 2
+box3 clicked 3
+box2 clicked 3
+box1 clicked 3
+
+when you click on box 3, the output will be:
+box3 clicked 2
+box2 clicked 3
+box1 clicked 3
+
+*/
---
+++ Events deep dive/3. Event propagation/main_v4.js
@@ -0,0 +1,49 @@
+const box1 = document.querySelector('.box1')
+const box2 = document.querySelector('.box2')
+const box3 = document.querySelector('.box3')
+const box4 = document.querySelector('.box4')
+
+box1.addEventListener(
+  'click',
+  e => console.log('box1 clicked', e.eventPhase),
+  true,
+)
+
+box2.addEventListener(
+  'click',
+  e => console.log('box2 clicked', e.eventPhase),
+  true,
+)
+
+box3.addEventListener(
+  'click',
+  e => console.log('box3 clicked', e.eventPhase),
+  true,
+)
+
+box4.addEventListener(
+  'click',
+  e => console.log('box4 clicked', e.eventPhase),
+  true,
+)
+
+/*
+NOTES on e.eventPhase:
+1 = capturing 
+2 = target
+3 = bubbling
+
+when usecapture argument is true then when you click on a child box, capturing will occur from parent to child
+
+when you click on box 4, the output will be:
+box1 clicked 1
+box2 clicked 1
+box3 clicked 1
+box4 clicked 2
+
+when you click on box 3, the output will be:
+box1 clicked 1
+box2 clicked 1
+box3 clicked 2
+
+*/
