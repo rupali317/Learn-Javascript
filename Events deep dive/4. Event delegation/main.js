ce1f1edaf0b261790442f6507afc15eb6c7bc185
rupali317 <rupali317@gmail.com>
2026-01-10 21:29:05 +0800

    [Event delegation] Jot some notes

--- Events deep dive/4. Event delegation/main.js
+++ Events deep dive/4. Event delegation/main.js
@@ -2,8 +2,10 @@ const list = document.querySelector('ul')
 const button = document.querySelector('button')
 
 list.addEventListener('click', e => {
+  console.log('list clicked')
+  console.log('e.target = ', e.target) // returns the element where the event occurs
+  console.log('e.currentTarget = ', e.currentTarget) // returns wthe element where the event listener is attached
   if (e.target.matches('li')) console.log(e.target)
-  //console.log(e.target) // will log other like li, a, ul
 })
 
 list.addEventListener('click', e => {
@@ -15,5 +17,5 @@ list.addEventListener('click', e => {
 button.addEventListener('click', logging)
 
 function logging(e) {
-  console.log(e.target)
+  console.log(e.target) // will log button htmlbuttonelement since css has pointer-event set as none. without none it may print svg or the text depending on where we are clicking
 }
