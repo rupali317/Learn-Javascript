e758b1f20862a1c2a16bbb4b2e00c032c7ea600a
rupali317 <rupali317@gmail.com>
2026-01-11 13:48:50 +0800

    [Modal: Closing the modal] Implemented with stopPropagation as well , made corrections to the implementation in method 1

--- Events deep dive/6. Modal: Closing the modal/js/main_1.js
+++ Events deep dive/6. Modal: Closing the modal/js/main_1.js
@@ -10,11 +10,21 @@ jsModalButton.addEventListener('click', ManageVisibilityModal)
 jsModalClose.addEventListener('click', ManageVisibilityModal)
 
 // when the overlay is clicked then we close otherwise if the pink modal is clicked then the modal should not be clicked
+// Method 1: not using e.stopPropagation()
 jsModalOverlay.addEventListener('click', e => {
-  if (!e.target.matches('.modal')) {
+  if (!e.target.closest('.modal')) {
     jsModalOverlay.classList.remove('open-mode')
   }
 })
+
+// Method 2: e.stopPropagation()
+/*jsModalOverlay.addEventListener('click', e => {
+  if (e.target.closest('.modal')) {
+    e.stopPropagation()
+  } else {
+    jsModalOverlay.classList.remove('open-mode')
+  }
+})*/
 
 function ManageVisibilityModal() {
   jsModalOverlay.classList.toggle('open-mode')
