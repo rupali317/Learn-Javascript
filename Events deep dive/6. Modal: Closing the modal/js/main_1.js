566ed79fefee2ea44e4c8c2f24c0a837040d9f07
rupali317 <rupali317@gmail.com>
2026-01-11 13:38:48 +0800

    [Closing the modal] Implemented the part where user clicks on the overlay as well

--- Events deep dive/6. Modal: Closing the modal/js/main_1.js
+++ Events deep dive/6. Modal: Closing the modal/js/main_1.js
@@ -9,6 +9,13 @@ jsModalButton.addEventListener('click', ManageVisibilityModal)
 
 jsModalClose.addEventListener('click', ManageVisibilityModal)
 
+// when the overlay is clicked then we close otherwise if the pink modal is clicked then the modal should not be clicked
+jsModalOverlay.addEventListener('click', e => {
+  if (!e.target.matches('.modal')) {
+    jsModalOverlay.classList.remove('open-mode')
+  }
+})
+
 function ManageVisibilityModal() {
   jsModalOverlay.classList.toggle('open-mode')
 }
