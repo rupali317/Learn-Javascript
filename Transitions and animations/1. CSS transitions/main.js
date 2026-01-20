9eedffce9aac9923bfe4ecdc059e74f724263b92
rupali317 <rupali317@gmail.com>
2026-01-20 22:52:56 +0800

    [CSS Transitions] Exercise

--- Transitions and animations/1. CSS transitions/main.js
+++ Transitions and animations/1. CSS transitions/main.js
@@ -1,1 +1,6 @@
 // Adding/Removing a class to button via javascript
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+  button.classList.toggle('is-active')
+})
