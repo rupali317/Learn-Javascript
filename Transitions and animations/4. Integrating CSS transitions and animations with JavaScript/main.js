8fa69bfdc2c38e14928a1aa0290b462142dcb26e
rupali317 <rupali317@gmail.com>
2026-01-23 09:59:54 +0800

    [4. Integrating CSS transitions and animations with JS] Experimenting more with transitionend and animationend and the event property

--- Transitions and animations/4. Integrating CSS transitions and animations with JavaScript/main.js
+++ Transitions and animations/4. Integrating CSS transitions and animations with JavaScript/main.js
@@ -7,16 +7,20 @@ const buttonFive = document.querySelector('#button-five')
 // Use transitionend to remove elements when their transition is completes.
 
 buttonOne.addEventListener('transitionend', () => {
-  buttonOne.remove()
+  buttonOne.remove() // removed from the DOM
 })
 
 buttonTwo.addEventListener('transitionend', () => {
-  buttonTwo.remove()
+  buttonTwo.setAttribute('style', 'display:none') // remains in the DOM
 })
 
 buttonThree.addEventListener('transitionend', () => {
   console.log('Button three is removed') // will be logged 4 times since this is about the borders on all 4 directions
   buttonThree.remove()
+})
+
+buttonThree.addEventListener('click', () => {
+  buttonThree.classList.add('is-highlighted')
 })
 
 buttonFour.addEventListener('animationend', () => {
@@ -25,4 +29,15 @@ buttonFour.addEventListener('animationend', () => {
 
 buttonFive.addEventListener('animationend', () => {
   buttonFive.remove()
+})
+
+document.addEventListener('transitionend', e => {
+  console.log('transition e.target', e.target) // e is TransitionEvent
+  console.log('transition e.propertyName', e.propertyName) // The property that was transitioned
+  // e will print all properties that are transitioned like color, border-bottom-width, border-left-width, border-right-width, border-top-width
+})
+
+document.addEventListener('animationend', e => {
+  console.log('animation e.target', e.target) // e is AnimationEvent
+  console.log('animation e.propertyName', e.animationName)
 })
