8c8d6ba965b0fc45d82b42ab55a554a8bd525f60
rupali317 <rupali317@gmail.com>
2026-02-01 21:54:20 +0800

    [Transitions and animations: Off canvas menu] writing notes on why animation is happening on body with :has()

--- Transitions and animations/7. Off-canvas menu/js/main_v1.js
+++ Transitions and animations/7. Off-canvas menu/js/main_v1.js
@@ -1,9 +1,5 @@
 const button = document.querySelector('button')
 const menu = document.querySelector('menu')
-const main = document.querySelector('main')
-const body = document.body
 button.addEventListener('click', function () {
-  // menu.classList.toggle('menu-closed')
-  // main.classList.toggle('menu-closed')
-  body.classList.toggle('menu-closed')
+  menu.classList.toggle('menu-closed')
 })
