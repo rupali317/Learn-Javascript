a485505e867e5fb99818555e38be53762ae9a173
rupali317 <rupali317@gmail.com>
2026-02-01 20:12:19 +0800

    Experiment with module in JS

---
+++ [Rupali practice] Experiment with module/js/main_1.js
@@ -0,0 +1,3 @@
+import { fName } from './main_2.js'
+
+console.log("main_2's fName", fName())
---
+++ [Rupali practice] Experiment with module/js/main_2.js
@@ -0,0 +1,6 @@
+const constVar = 2
+
+export function fName() {
+  // return constVar
+  console.log(constVar)
+}
