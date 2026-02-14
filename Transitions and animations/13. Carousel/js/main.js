17c80975c6df30046bee7cf43f5b9433da139702
rupali317 <rupali317@gmail.com>
2026-02-14 19:09:34 +0800

    [Transitions and animations] Carousel - TODO: Do an improvement

---
+++ Transitions and animations/13. Carousel/js/main.js
@@ -0,0 +1,74 @@
+const prevBtn = document.getElementById('prev-btn')
+const nextBtn = document.getElementById('next-btn')
+const carouselDots = document.querySelector('.carousel__dots')
+const carouselContents = document.querySelector('.carousel__contents')
+const carouselSlideList = carouselContents.querySelectorAll('.carousel__slide')
+const carouselDotList = Array.from(
+  carouselDots.querySelectorAll('.carousel__dot'),
+)
+let currentSlide = carouselContents.querySelector('.js-is-selected')
+let currentDot = carouselDots.querySelector('.js-is-selected')
+
+carouselSlideList.forEach((carouselSlide, index) => {
+  carouselSlide.style.left = index * getWidthOfSlideContainer() + 'px'
+})
+
+prevBtn.addEventListener('click', () => {
+  if (currentSlide.previousElementSibling !== null) {
+    shiftSlide(
+      currentSlide.previousElementSibling,
+      currentDot.previousElementSibling,
+    )
+  }
+  manageVisibilityOfNavigationButtons()
+})
+
+nextBtn.addEventListener('click', () => {
+  if (currentSlide.nextElementSibling !== null) {
+    shiftSlide(currentSlide.nextElementSibling, currentDot.nextElementSibling)
+  }
+  manageVisibilityOfNavigationButtons()
+})
+
+carouselDots.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+    currentSlide.classList.remove('js-is-selected')
+    currentDot.classList.remove('js-is-selected')
+    e.target.classList.add('js-is-selected')
+    currentDot = e.target
+    index = getIndexSelectedDot()
+    carouselSlideList[index].classList.add('js-is-selected')
+    currentSlide = carouselSlideList[index]
+    const left = getComputedStyle(carouselSlideList[index]).left
+    carouselContents.style.left = '-' + left
+    manageVisibilityOfNavigationButtons()
+  }
+})
+
+function getWidthOfSlideContainer() {
+  return carouselContents.getBoundingClientRect().width
+}
+
+function shiftSlide(targetSlide, targetDot) {
+  currentSlide.classList.remove('js-is-selected')
+  currentDot.classList.remove('js-is-selected')
+  targetSlide.classList.add('js-is-selected')
+  targetDot.classList.add('js-is-selected')
+  const left = getComputedStyle(targetSlide).left
+  carouselContents.style.left = '-' + left
+  currentSlide = targetSlide
+  currentDot = targetDot
+}
+
+function manageVisibilityOfNavigationButtons() {
+  const hasNext = currentSlide.nextElementSibling !== null
+  const hasPrev = currentSlide.previousElementSibling !== null
+  nextBtn.hidden = !hasNext
+  prevBtn.hidden = !hasPrev
+}
+
+function getIndexSelectedDot() {
+  return carouselDotList.findIndex(carouselDot => {
+    if (carouselDot.classList.contains('js-is-selected')) return true
+  })
+}
