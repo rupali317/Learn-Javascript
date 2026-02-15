c0d7064dd483de0c82c7cabd5a1c99c3ef6fbc23
rupali317 <rupali317@gmail.com>
2026-02-15 12:15:31 +0800

    [Transitions and animations] Made the animation work by controlling the height and ensuring other glitches do not happen

--- Transitions and animations/12. Accordion/js/main.js
+++ Transitions and animations/12. Accordion/js/main.js
@@ -11,8 +11,15 @@ const accordionContainer = document.querySelector('.accordion-container')
 // Method 2: Check if clicked in the header - Zell's approach (clear intent)
 accordionContainer.addEventListener('click', e => {
   const accordionHeader = e.target.closest('.accordion__header')
+  const accordion = accordionHeader.closest('.accordion')
+  const accordionContent = accordionHeader.nextElementSibling
   if (accordionHeader) {
     // accordionHeader.parentElement.classList.toggle('is-open') // If the HTML structure changes parentElement will be a different element
-    accordionHeader.closest('.accordion').classList.toggle('is-open') // My tweak
+    accordion.classList.toggle('is-open') // My tweak
+    if (accordion.classList.contains('is-open')) {
+      accordionContent.style.height = accordionContent.scrollHeight + 'px'
+    } else {
+      accordionContent.style.height = '0px'
+    }
   }
 })
